d8524271ec02e76370a945a7e58b5ea26e4a76c7
neon1024 <robertscumnpu@gmail.com>
2024-11-09 02:56:48 +0200

    Lab 3 -> Done?

---
+++ Labs/Lab 3/.local/install/CodedWorkflow.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.MicrosoftOffice365.Activities.Api;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+using UiPath.CodedWorkflows.DescriptorIntegration;
+
+namespace Lab2
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{typeof(UiPath.MicrosoftOffice365.Activities.Api.IOffice365ConnectionsService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+        }
+
+        protected UiPath.MicrosoftOffice365.Activities.Api.IOffice365ConnectionsService office365 { get => serviceContainer.Resolve<UiPath.MicrosoftOffice365.Activities.Api.IOffice365ConnectionsService>(); }
+
+        protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
+
+        protected UiPath.Testing.API.ITestingService testing { get => serviceContainer.Resolve<UiPath.Testing.API.ITestingService>(); }
+
+        protected UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService uiAutomation { get => serviceContainer.Resolve<UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService>(); }
+    }
+}
+
+namespace Lab2.ObjectRepository
+{
+    public static class Descriptors
+    {
+    }
+}
+
+namespace Lab2._Implementation
+{
+    internal class ScreenDescriptorDefinition : IScreenDescriptorDefinition
+    {
+        public IScreenDescriptor Screen { get; set; }
+
+        public string Reference { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+
+    internal class ElementDescriptorDefinition : IElementDescriptorDefinition
+    {
+        public IScreenDescriptor Screen { get; set; }
+
+        public string Reference { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public IElementDescriptor ParentElement { get; set; }
+
+        public IElementDescriptor Element { get; set; }
+    }
+}
